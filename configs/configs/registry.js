708736dc77c02f76d5f8d38afe6bc505439e988e
keepmeathirty-dot <keepmeathirty@gmail.com>
2026-09-26 18:16:04 +0200

    Add workspace registry

---
+++ configs/configs/registry.js
@@ -0,0 +1,15 @@
+window.AVVA_WORKSPACES = [
+  {
+    id: "kzn-doa",
+    label: "KZN Dept. of Agriculture",
+    description: "Farm support & funding accountability",
+    configPath: "configs/kzn-doa.js",
+    default: true
+  },
+  {
+    id: "hennessy",
+    label: "Hennessy Brand Intelligence",
+    description: "Product distribution & market insights",
+    configPath: "configs/hennessy.js"
+  }
+];
